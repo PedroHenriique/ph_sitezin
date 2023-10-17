0920312b3b254a4f8d0414562f61ddb7cf0bedac
Pedro Henrique <pedro@etssoft.com.br>
2023-10-17 17:55:21 -0300

    modalzin

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -9,9 +9,4 @@ declare var $: any;
 export class ModalComponent {
 
   @Input() modalId: string = "modal";
-
-  closeModal() {
-    console.log("funcionando")
-    $("#" +  this.modalId).modal('toggle'); // Use jQuery or Angular equivalent
-  }
 }
